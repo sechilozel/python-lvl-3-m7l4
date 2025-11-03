8479d1e46ed6bd67dda0bb2e3540666a54f3c914
sechilozel <secilozel@hotmail.com>
2025-11-03 21:22:49 +0300

    Added 5 more unit tests

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -36,11 +36,52 @@ def test_add_new_user(setup_database, connection):
     user = cursor.fetchone()
     assert user, "Kullanıcı veri tabanına eklenmiş olmalıdır."
 
+def test_same_username(setup_database, connection):
+    ao = add_user('a', 'ao@gmail.com', 'asd123')
+    ai = add_user('a', 'ai@gmail.com', 'oshinoko123')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='a';")
+    user = cursor.fetchone()
+    assert user, "Kullanıcı eklenmemiş olmalıdır."
+
+def test_authenticate_success(setup_database, connection):
+    add_user('Mira', 'mira1512@hotmail.com', 'cecilia1512')
+    authenticate_user('Mira', 'cecilia1512')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='Mira';")
+    user = cursor.fetchone()
+    assert user, "Kullanıcı doğrulanmış olmalıdır."
+
+def test_wrong_password(setup_database, connection):
+    add_user('Mira', 'mira1512@hotmail.com', 'cecilia1512')
+    authenticate_user('Mira', 'cecliia1512')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='Mira';")
+    user = cursor.fetchone()
+    assert user, "Kullanıcı doğrulanmamış olmalıdır."
+
+def test_userlist_success(setup_database, connection):
+    add_user('Mira', 'mira1512@hotmail.com', 'cecilia1512')
+    add_user('Emre', 'emrei@gmail.com', 'mira1512emre')
+    display_users()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users;")
+    user = cursor.fetchone()
+    assert user, "Kullanıcıların listesi listelenmiş olmalıdır."
+
+def test_nonexistentuser_authentication(setup_database, connection):
+    authenticate_user('Mira', 'cecilia1512')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username='Mira';")
+    user = cursor.fetchone()
+    assert user, "Kullanıcı bulunamamış olmalıdır."
+
+
 # İşte yazabileceğiniz bazı testler:
 """
-Var olan bir kullanıcı adıyla kullanıcı eklemeye çalışmayı test etme.
-Başarılı kullanıcı doğrulamasını test etme.
+Var olan bir kullanıcı adıyla kullanıcı eklemeye çalışmayı test etme. +??
+Başarılı kullanıcı doğrulamasını test etme. ++?
 Var olmayan bir kullanıcıyla doğrulama yapmayı test etme.
-Yanlış şifreyle doğrulama yapmayı test etme.
-Kullanıcı listesinin doğru şekilde görüntülenmesini test etme.
+Yanlış şifreyle doğrulama yapmayı test etme. +++
+Kullanıcı listesinin doğru şekilde görüntülenmesini test etme. +++
 """
